bf98c19ff8766968355b185c60152eead7ec1eca
M1K00 <61482244+M1K00@users.noreply.github.com>
2020-05-27 09:54:19 -0400

    Added Post

--- TMR.Data/IdentityModels.cs
+++ TMR.Data/IdentityModels.cs
@@ -1,4 +1,6 @@
 ﻿using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
@@ -28,6 +30,33 @@ namespace TMR.Data
         public static ApplicationDbContext Create()
         {
             return new ApplicationDbContext();
+        }
+
+        public DbSet<Post> Posts { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder
+         .Conventions
+         .Remove<PluralizingTableNameConvention>();
+
+            modelBuilder
+                .Configurations
+                .Add(new IdentityUserLoginConfiguration())
+                .Add(new IdentityUserRoleConfiguration());
+        }
+    }
+    public class IdentityUserLoginConfiguration : EntityTypeConfiguration<IdentityUserLogin>
+    {
+        public IdentityUserLoginConfiguration()
+        {
+            HasKey(iul => iul.UserId);
+        }
+    }
+    public class IdentityUserRoleConfiguration : EntityTypeConfiguration<IdentityUserRole>
+    {
+        public IdentityUserRoleConfiguration()
+        {
+            HasKey(iur => iur.UserId);
         }
     }
 }
---
+++ TMR.Data/Post.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMR.Data
+{
+    public class Post
+    {
+        [Key] public int ID { get; set; }
+        [Required] public Guid UserID { get; set; }
+        [Required] public string Title { get; set; }
+        [Required] public string Body { get; set; }
+        [Required] public DateTimeOffset TimePosted { get; set; }
+        public DateTimeOffset? TimeEdited { get; set; }
+        public int Votes { get; set; }
+        public List<Guid> Voters { get; set; }
+        public bool Solved { get; set; } = false;
+    }
+}
